502c1c701c9e2c33b5e860b9b9eba2da2f881359
JobCollins <dulo.collins@gmail.com>
2020-06-29 20:49:08 +0300

    feature: modularization of the code
    this commit is the modularization of the code in the Untitled.ipynb jupyter notebook file, The code has been modularized into processing.py, split_data.py, and model.py

---
+++ app/model/split_data.py
@@ -0,0 +1,29 @@
+from processing import readFile
+
+import matplotlib.pyplot as plt
+
+
+new_data = readFile()
+
+def split_set():
+
+    train_date = date[:int(len(new_data)*0.75)]
+    train = new_data[:int(len(new_data)*0.75)].copy()
+
+    test_date = date[int(len(new_data)*0.75):]
+    test = new_data[int(len(new_data)*0.75):].copy()
+
+    print(f'Train shape {train.shape}, Test shape {test.shape}')
+
+    return train_date, train, test_date, test
+
+def plot_split_set(name):
+
+    train_date, train, test_date, test = split_set()    
+    
+    plt.figure(figsize=(16,4))
+
+    plt.plot(train_date, train[name], label='train')
+    plt.plot(test_date, test[name], label='test')
+    plt.ylabel(name); plt.legend()
+    plt.show()
